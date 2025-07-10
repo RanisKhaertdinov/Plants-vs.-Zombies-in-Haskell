17ed8c08cbf0dd64640465462e1ab647032c63ba
MikhailAkhatov <m.akhatov@innopolis.university>
2025-07-10 18:41:25 +0300

    Update Plant.hs

--- app/Plant.hs
+++ app/Plant.hs
@@ -10,6 +10,21 @@ data Plant  -- (dx, dy) health
 data PlantType
     = Sunflower
     | Peashooter
+    | WallNut
+    deriving (Eq, Show)
+    
+data PlantCard = PlantCard
+    { cardType :: PlantType
+    , cost :: Int
+    , cooldown :: Int
+    }
+
+availableCards :: [PlantCard]
+availableCards =
+    [ PlantCard Sunflower 50 7
+    , PlantCard Peashooter 100 5
+    , PlantCard WallNut 50 20
+    ]
 
 
 generatePlant :: Plant -> Picture 
@@ -21,7 +36,10 @@ generatePlant (Plant Peashooter (x, y) health) =
     if health > 0 
     then Translate x y $ Color green $ circleSolid 30
     else blank
-
+generatePlant (Plant WallNut (x, y) health) =
+    if health > 0
+    then Translate x y $ Color (makeColor 0.5 0.3 0.1 1.0) $ rectangleSolid 40 60
+    else blank
 
 
 
