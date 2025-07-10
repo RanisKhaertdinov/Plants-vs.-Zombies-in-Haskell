17fed565759b6510bf9a90e378aa0a103cb26a38
MikhailAkhatov <m.akhatov@innopolis.university>
2025-07-10 19:33:53 +0300

    Update GameStates.hs

--- app/GameStates.hs
+++ app/GameStates.hs
@@ -1,6 +1,4 @@
 module GameStates where
 
-import Plant (PlantType(..))
-
-data GameState = Playing Float | GameOver | SelectingPlant PlantType
+data GameState = Playing Float | GameOver
     deriving (Show)
