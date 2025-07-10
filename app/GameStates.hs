d1d4b42a56c6405b188e53efb98c2f99604c0be9
Mikhail <m.akhatov@innopolis.university>
2025-07-10 23:17:15 +0300

    Saved locally

--- app/GameStates.hs
+++ app/GameStates.hs
@@ -3,7 +3,7 @@ module GameStates where
 import Plant
 
 data GameState 
-    = Playing [Plant] Float 
+    = Playing [Plant] Float
     | GameOver 
     | SelectingPlant [Plant] Float PlantType
     deriving (Show)
